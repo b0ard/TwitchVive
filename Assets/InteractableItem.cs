12f906fcb00db27c12ebfd5041b0ae61838846c6
Sean Lee <slee@alumni.ubc.ca>
2016-03-09 23:41:43 -0800

    Basic physics based interaction
    -Mass sensitive direct velocity manipulation of rigidbodies
    -Mass sensitive rotation via angular velocity
    -Transfer between two hands
    -Get closest object

---
+++ Assets/InteractableItem.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableItem : MonoBehaviour {
+    public Rigidbody rigidbody;
+
+    [SerializeField]
+    private bool currentlyInteracting;
+
+    [SerializeField]
+    private WandController attachedWand;
+
+    [SerializeField]
+    private float velocityFactor = 20000f;
+
+    [SerializeField]
+    private float rotationFactor = 400f;
+
+    // For determining position
+    private Vector3 posDelta;
+
+    // For determining rotation
+    private Quaternion rotationDelta;
+    private float angle;
+    private Vector3 axis;
+
+    private Transform interactionPoint;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        velocityFactor /= rigidbody.mass;
+        rotationFactor /= rigidbody.mass;
+
+        interactionPoint = new GameObject().transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (attachedWand) {
+            // TODO: maybe configurable joint
+            posDelta = attachedWand.transform.position - interactionPoint.position; 
+            this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+            rotationDelta = attachedWand.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+            rotationDelta.ToAngleAxis(out angle, out axis);
+
+            if (angle > 180) {
+                angle -= 360;
+            }
+
+            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+        }
+    }
+
+    public void BeginInteraction(WandController wand) {
+        attachedWand = wand;
+        interactionPoint.position = wand.transform.position;
+        interactionPoint.rotation = wand.transform.rotation;
+        interactionPoint.SetParent(transform, true);
+
+        currentlyInteracting = true;
+    }
+
+    public void EndInteraction(WandController wand) {
+        if (wand == attachedWand) {
+            attachedWand = null;
+            currentlyInteracting = false;
+        }
+    }
+
+    public bool isInteracting() {
+        return currentlyInteracting;
+    }
+}
---
+++ Assets/WandController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WandController : MonoBehaviour {
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)_trackedObj.index); } }
+    private SteamVR_TrackedObject _trackedObj;
+
+    private HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
+
+    [SerializeField]
+    private InteractableItem closestItem = null;
+    [SerializeField]
+    private InteractableItem interactingItem = null;
+
+    void Awake() {
+        _trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller == null) {
+            Debug.Log("Controller is null");
+            return;
+        }
+
+        if (controller.GetPressDown(gripButton)) {
+            float minDistance = float.MaxValue;
+
+            float distance;
+            foreach (InteractableItem item in objectsHoveringOver) {
+                distance = (item.transform.position - transform.position).sqrMagnitude;
+
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestItem = item;
+                }
+            }
+
+            interactingItem = closestItem;
+            if (interactingItem) {
+                if (interactingItem.isInteracting()) {
+                    interactingItem.EndInteraction(this);
+                }
+
+                interactingItem.BeginInteraction(this);
+            }
+            closestItem = null; // TODO: should this be here?
+        }
+
+        if (controller.GetPressUp(gripButton) && interactingItem) { 
+            interactingItem.EndInteraction(this);
+        }
+	}
+
+    private void OnTriggerEnter(Collider collider) {
+        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+        if (collidedItem) {
+            objectsHoveringOver.Add(collidedItem);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+
+        if (collidedItem) {
+            objectsHoveringOver.Remove(collider.GetComponent<InteractableItem>());
+        }
+    }
+}
